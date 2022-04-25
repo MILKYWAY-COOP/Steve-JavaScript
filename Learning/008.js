9108e3f28a5f3c70b4226ceaf4461f4bd99bc267
Stephen Muchendu <muchendumaina66@gmail.com>
2022-04-25 17:15:01 +0300

    OOPs

--- Learning/008.js
+++ Learning/008.js
@@ -11,3 +11,15 @@ var p = new person("David", 21);
 p.changeName("Stephen")
 
 console.log(p.age)
+
+// its is also possible to define function outside of a method
+function person(name, age) {
+    this.name= name;
+    this.age = age;
+    this.yearOfBirth = bornYear;
+  }
+  function bornYear() {
+    return 2022 - this.age;
+  }
+var Steve = new person("Muchendu", 21)
+console.log(Steve.yearOfBirth)
--- Learning/tempCodeRunnerFile.js
+++ Learning/tempCodeRunnerFile.js
@@ -1,6 +1,10 @@
-var lion = {
-    name: "Simba",
-    age: 3
-}
-
-console.log(lion.name)
+function person(name, age) {
+    this.name= name;
+    this.age = age;
+    this.yearOfBirth = bornYear;
+  }
+  function bornYear() {
+    return 2022 - this.age;
+  }
+var Steve = new person("Muchendu", 21)
+console.log(Steve.bornYear)
