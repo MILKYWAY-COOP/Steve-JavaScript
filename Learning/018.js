42e436de43cd61e003a45e5df137597d5e9b74c8
Stephen Muchendu <muchendumaina66@gmail.com>
2022-04-27 15:56:29 +0300

    More Maps

--- Learning/018.js
+++ Learning/018.js
@@ -1,3 +1,12 @@
 let map = new Map([['k1', 'v1'], ['k2', 'v2']]);
 
 console.log(map.size); // 2
+
+let mapp = new Map();
+mapp.set('k1', 'v1').set('k2', 'v2');
+
+console.log(mapp.get('k1')); // v1
+console.log(mapp.has('k2')); // true
+
+for (let kv of mapp.entries())
+    console.log(kv[0] + " : " + kv[1]);
