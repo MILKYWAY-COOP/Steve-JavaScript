f9ef45926ea8090934ea17196f6aced434a73320
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-09 14:42:46 +0300

    E6 Variables

---
+++ EcmaScript/variables.js
@@ -0,0 +1,12 @@
+var names = "Brenda"
+names = 'Tom'
+alert(names);
+
+//ECMAScript
+//Const
+const name = 'Brenda';
+names = 'Tom'; // will return an error because value is not mutable
+alert(names);
+
+//let
+
