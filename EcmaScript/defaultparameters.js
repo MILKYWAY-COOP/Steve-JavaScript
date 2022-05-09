9fe573143ecee963c49f8917b95f3616a5f3d9d4
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-09 15:09:16 +0300

    Working with default parameters

---
+++ EcmaScript/defaultparameters.js
@@ -0,0 +1,12 @@
+// function welcome(user, message) {
+//     alert(`Hello ${user}, ${message}`);
+// }
+
+// welcome("Ryan", "Good morning");
+//welcome() -> The function will crush if no input is provided
+
+function welcome(user="Mystery person", message="Goodday") {
+    alert(`Hello ${user}, ${message}`);
+}
+
+welcome();
